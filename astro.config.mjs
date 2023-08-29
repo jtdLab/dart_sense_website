5f1b25637512801716d88db29771037e1f18ae6d
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-08-29 20:47:10 +0200

    sdfno

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jtdLab.github.io',
-  base: 'pizza',
+  base: 'dart_sense_website',
   integrations: [react(), tailwind()],
 });
