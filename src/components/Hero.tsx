65edeebd2e8429cb1568d883f30059fbe69b66e5
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-01-23 06:16:37 +0100

    fix: #4

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,11 +12,11 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0 }}
         >
-          <div className="text-7xl lg:text-8xl xl:text-8xl font-extrabold tracking-wide text-white px-8 sm:px-8 md:px-20 lg:px-4 sm:mt-32 mt-16">
-            <div className="text-7xl lg:text-8xl xl:text-8xl font-extrabold tracking-wide text-white px-8 sm:px-8 md:px-20 lg:px-4">
+          <div className="text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-8xl font-extrabold tracking-wide text-white px-8 sm:px-8 md:px-20 lg:px-4 sm:mt-32 mt-16">
+            <div className="text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-8xl font-extrabold tracking-wide text-white px-8 sm:px-8 md:px-20 lg:px-4">
               <span className="">DartSense</span>
             </div>
-            <span className="bg-gradient-to-r text-transparent bg-clip-text from-blue-400 to-orange-500 via-purple-500 animate-gradient-xy inline text-7xl lg:text-8xl xl:text-8xl">
+            <span className="bg-gradient-to-r text-transparent bg-clip-text from-blue-400 to-orange-500 via-purple-500 animate-gradient-xy inline text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-8xl">
               VoicePlay
             </span>
           </div>
