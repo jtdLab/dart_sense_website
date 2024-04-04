55afa0d81bcc48021830d52d917f9c011871f92e
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-04-04 12:24:03 +0200

    fix: #6

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import astroI18next from 'astro-i18next';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dartsense.com',
+  i18n: {
+    defaultLocale: 'de',
+    locales: ['de', 'en'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
   integrations: [react(), tailwind(), astroI18next()],
 });
