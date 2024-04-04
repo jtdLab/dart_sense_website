9d696db0875e7b06feaf10eac6e698186dd68636
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-04-04 14:14:03 +0200

    fix: try fix redirect placholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import astroI18next from 'astro-i18next';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dartsense.com',
+  build: {
+    redirects: false,
+  },
   i18n: {
     defaultLocale: 'de',
     locales: ['de', 'en'],
