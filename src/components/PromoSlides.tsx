0ad15023a1fb8f00184b7515997b533b2ba40b44
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-04-07 11:39:50 +0200

    fix: slider

--- src/components/PromoSlides.tsx
+++ src/components/PromoSlides.tsx
@@ -1,6 +1,12 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
+import { useState, useEffect } from 'react';
+import {
+  CarouselProvider,
+  DotGroup,
+  Dot,
+  Slide,
+  Slider,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 interface Props {
   title: string;
@@ -8,47 +14,69 @@ interface Props {
 }
 
 export const PromoSlides = (props: Props) => {
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-    ],
-  };
+  const [visibleSlides, setVisibleSlides] = useState(3);
+
+  useEffect(() => {
+    const updateVisibleSlides = () => {
+      if (window.innerWidth <= 768) {
+        setVisibleSlides(1);
+      } else if (window.innerWidth <= 1200) {
+        setVisibleSlides(2);
+      } else {
+        setVisibleSlides(3);
+      }
+    };
+
+    updateVisibleSlides();
+
+    window.addEventListener('resize', updateVisibleSlides);
+
+    return () => {
+      window.removeEventListener('resize', updateVisibleSlides);
+    };
+  }, []);
 
   return (
     <div className="py-16">
       <div className="dartsense-block-big-title text-center mb-16 px-8 sm:px-24 md:px-48">
         {props.title}
       </div>
-      <Slider {...settings}>
-        {props.images.map((imageSrc, index) => (
-          <div key={index} className="xxx">
-            <img
-              src={imageSrc}
-              alt={`Image ${index}`}
-              className="rounded-l h-[500px] xs:h-[600px]"
-            />
-          </div>
-        ))}
-      </Slider>
+      <CarouselProvider
+        naturalSlideWidth={944}
+        naturalSlideHeight={1616}
+        totalSlides={props.images.length}
+        isIntrinsicHeight={true}
+        isPlaying={true}
+        interval={4000}
+        visibleSlides={visibleSlides}
+        step={visibleSlides}
+        dragStep={visibleSlides}
+      >
+        <Slider>
+          {props.images.map((imageSrc, index) => (
+            <Slide index={index} key={index}>
+              <div className="promo-slides-item">
+                <img
+                  src={imageSrc}
+                  alt={`Image ${index}`}
+                  className="rounded-l h-[500px] xs:h-[600px]"
+                />
+              </div>
+            </Slide>
+          ))}
+        </Slider>
+        <div className="prc-dotGroup">
+          {[...Array(Math.ceil(props.images.length / visibleSlides))].map(
+            (_, index) => (
+              <Dot
+                key={index}
+                slide={index * visibleSlides}
+                className={`prc-dot ${true ? 'prc-dot--selected' : ''}`}
+              />
+            ),
+          )}
+        </div>
+      </CarouselProvider>
     </div>
   );
 };
