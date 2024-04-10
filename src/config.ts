e983dcec54c144d7ef438eee3f8d635067a5d38b
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-04-10 16:20:04 +0200

    feat: add links

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   app: 'DartSense',
-  discordUrl: 'https://discord.com/invite/wwpvAnHYMj',
+  discordUrl: 'https://discord.gg/39qqVdMW',
   instagramUrl: 'https://instagram.com/dartsense',
   appStoreUrl: 'https://apps.apple.com/de/app',
   playStoreUrl: 'https://play.google.com/store/apps',
