48ef45c6c048b88e11d9f0fa277334e6f6364508
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-05-12 12:01:30 +0200

    fix: discord link

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   app: 'DartSense',
-  discordUrl: 'https://discord.gg/39qqVdMW',
+  discordUrl: 'https://discord.gg/3mZdav6r',
   instagramUrl: 'https://instagram.com/dartsense',
   appStoreUrl: 'https://apps.apple.com/de/app',
   playStoreUrl: 'https://play.google.com/store/apps',
