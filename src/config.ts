77cab361cf4ebefc4f56cb00216d2df773083a22
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-07-29 21:19:27 +0200

    update google play store url

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const config = {
   discordUrl: 'https://discord.gg/nM7NBemew9',
   instagramUrl: 'https://instagram.com/dartsense',
   appStoreUrl: 'https://apps.apple.com/de/app',
-  playStoreUrl: 'https://play.google.com/store/apps',
+  playStoreUrl: 'https://play.google.com/store/apps/details?id=com.jtdlab.dart_sense',
   consoleUrl: '',
   contactEmail: 'contact@dartsense.com',
 };
