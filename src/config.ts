8728022d7cfee147d2044dafef65a13f8ba3f518
jtdLab <72231111+jtdLab@users.noreply.github.com>
2024-10-18 15:14:05 +0200

    update links

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@ export const config = {
   app: 'DartSense',
   discordUrl: 'https://discord.gg/nM7NBemew9',
   instagramUrl: 'https://instagram.com/dartsense',
-  appStoreUrl: 'https://apps.apple.com/de/app',
+  appStoreUrl:
+    'https://apps.apple.com/de/app/dartsense-darts-spracheingabe/id6470947519',
   playStoreUrl:
     'https://play.google.com/store/apps/details?id=com.jtdlab.dart_sense',
   consoleUrl: '',
